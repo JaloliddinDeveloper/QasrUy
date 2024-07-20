fadb11a8be674ee0a6ee5f6e02f45bfa63b0a6f5
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-07-20 22:28:52 +0500

    FOUNDATIONS: CRUD House and Picture

--- QasrUy.Api/Migrations/StorageBrokerModelSnapshot.cs
+++ QasrUy.Api/Migrations/StorageBrokerModelSnapshot.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using QasrUy.Api.Brokers.Storages;
 
-#nullable disable
 
 namespace QasrUy.Api.Migrations
 {
---
+++ QasrUy.Api/Services/Foundations/PictureServices/PictureService.cs
@@ -0,0 +1,30 @@
+﻿using QasrUy.Api.Brokers.Storages;
+using QasrUy.Api.Models.Pictures;
+
+namespace QasrUy.Api.Services.Foundations.PictureServices
+{
+    public class PictureService: IPictureService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public PictureService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<Picture> AddPictureAsync(Picture picture)=>
+            await this.storageBroker.InsertPictureAsync(picture);
+
+        public IQueryable<Picture> RetrieveAllPictures()=>
+            storageBroker.SelectAllPicture();
+       
+        public async ValueTask<Picture> RetrievePictureByIdAsync(int pictureId)=>
+            await this.storageBroker.SelectByIdPictureAsync(pictureId);
+       
+        public async ValueTask<Picture> ModifyPictureAsync(Picture picture) =>
+            await this.storageBroker.UpdatePictureAsync(picture);
+      
+        public async ValueTask<Picture> RemovePictureAsync(Picture picture)=>
+            await this.storageBroker.DeletePictureAsync(picture);
+    }
+}
