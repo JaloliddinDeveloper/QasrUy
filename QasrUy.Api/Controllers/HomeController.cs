44fcccf8a53292461d24db310677ad10d29efd14
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-07-20 21:49:11 +0500

    INFRA:Api Project Initilalize

---
+++ QasrUy.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace QasrUy.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+       public ActionResult<string> Get()=>
+            Ok("Hello, Mario. The princes is in another castle.");
+    }
+}
---
+++ QasrUy.Api/Models/Houses/House.cs
@@ -0,0 +1,24 @@
+﻿using QasrUy.Api.Models.Pictures;
+
+namespace QasrUy.Api.Models.Houses
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int SelectedRoom { get; set; }
+        public double HomeSquare { get; set; }  
+        public int HomeFloor { get; set; }  
+        public int HomeAllFloor { get; set; }
+        public string SelectedAmenities { get; set; }   
+        public string HomePrice { get; set; }   
+        public string HomeRegion { get; set; }
+        public string HomeDistrict { get; set; }
+        public string HomeAddress { get; set; }
+        public string HomeLocationAddress { get; set; }
+        public string HomeLocationLatLng { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Picture> Pictures { get; set; } 
+    }
+}
